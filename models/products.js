b609db2791662a563b9b2cc708e62bd3a271eda0
Caio <caiopadilha@id.uff.br>
2022-08-11 15:34:11 -0300

    getById model

--- models/products.js
+++ models/products.js
@@ -7,6 +7,14 @@ const getAll = async () => {
   return products;
 };
 
+const getById = async (id) => {
+  const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
+  const [product] = await connection.execute(query, [id]);
+ 
+  return product;
+};
+
 module.exports = {
   getAll,
+  getById,
 };
