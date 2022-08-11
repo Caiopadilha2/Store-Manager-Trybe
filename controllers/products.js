23f4dafb2418eafc843b393be4a5e891e2d5ac40
Caio <caiopadilha@id.uff.br>
2022-08-11 15:35:17 -0300

    getById controller

--- controllers/products.js
+++ controllers/products.js
@@ -10,6 +10,22 @@ const listAll = async (_req, res, next) => {
   }
 };
 
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+  
+    const product = await productsModel.getById(id);
+
+    if (!product.length) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    return res.status(200).json(product[0]);
+  } catch (e) {
+    next(e);
+  }
+};
+
 module.exports = {
   listAll,
+  getById,
 };
