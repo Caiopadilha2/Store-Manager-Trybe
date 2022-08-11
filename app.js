523f2ec60bcaacc0eec63aa6c547663383332acc
Caio <caiopadilha@id.uff.br>
2022-08-11 15:38:28 -0300

    endpoint getproducts getByID

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.get('/', (_request, response) => {
 
 app.get('/products', productsController.listAll);
 
+app.get('/products/:id', productsController.getById);
+
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
