39e74d23a8e60481ff05a9a50977bfe5deb82531
Caio <caiopadilha@id.uff.br>
2022-08-13 16:27:23 -0300

    service sales getById

--- services/sales.js
+++ services/sales.js
@@ -2,6 +2,12 @@ const salesModel = require('../models/sales');
 
 const getAll = () => salesModel.getAll();
 
+const getById = async (id) => {
+  const sale = await salesModel.getById(id);
+  return sale;
+};
+
 module.exports = {
   getAll,
+  getById,
 };
